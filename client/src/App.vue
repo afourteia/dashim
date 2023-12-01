02d3dcb730372fe6f866f9e96c03c4a1a0d403fd
afourteia <raoph40@gmail.com>
2023-12-02 01:49:43 +0200

    Ran into difficulty integrating using nestjs

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,9 +3,12 @@
 </template>
 
 <script lang="ts">
+// import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+// import type { AppRouter } from '../path/to/server/trpc';
 import { defineComponent } from 'vue';
+// import { VueQueryPlugin } from "@tanstack/vue-query";
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
 });
 </script>
