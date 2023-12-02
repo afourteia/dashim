96d75065bb22620e307b7416321447da825ff9d3
afourteia <raoph40@gmail.com>
2023-12-02 02:01:42 +0200

    start fresh

--- server/src/common/trpc/trpc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrpcService } from './trpc.service';
-import { UserModule } from '../../models/user/user.module';
-
-@Module({
-  imports: [UserModule],
-  providers: [TrpcService],
-})
-export class TrpcModule {}
