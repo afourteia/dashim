4d950c13808e79b7bc6178615c5908b0e28da6e3
afourteia <raoph40@gmail.com>
2023-12-07 01:17:20 +0200

    configuring tailwind

---
+++ remix.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  // cacheDirectory: "./node_modules/.cache/remix", // from indie stack
+  ignoredRouteFiles: ['**/.*', '**/*.test.{ts,tsx}'],
+  tailwind: true,
+  postcss: true,
+  // serverModuleFormat: "cjs", // from indie stack
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+}
