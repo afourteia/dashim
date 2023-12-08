23375bfddd3f81aa3466c8fa4a3e30e18b29187a
afourteia <raoph40@gmail.com>
2023-12-08 15:25:49 +0200

    configured cors globally

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,9 +4,16 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react'
+import { startTransition, StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+
+import { cacheAssets } from 'remix-utils/cache-assets'
+
+cacheAssets().catch((error) => {
+  // do something with the error, or not
+  console.log('cacheAssets error', error)
+})
 
 startTransition(() => {
   hydrateRoot(
@@ -14,5 +21,5 @@ startTransition(() => {
     <StrictMode>
       <RemixBrowser />
     </StrictMode>
-  );
-});
+  )
+})
