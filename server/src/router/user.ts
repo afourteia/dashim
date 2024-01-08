c7e3b834c0fb1bfc0d22db89d033e7839873c8cb
jabah <mo.jabah0@gmail.com>
2024-01-08 21:02:01 +0200

    add patientsServices zenstack model

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -1,42 +1,105 @@
-import { router, publicProcedure } from './_trpc'
-import { z } from 'zod'
-import { User, type UserType } from '@models/user'
-import { TRPCError } from '@trpc/server'
-import ServerError from '~/utilities/error'
+import { router, publicProcedure } from "./_trpc";
+import { z } from "zod";
+import { User, type UserType } from "@models/user";
+import { TRPCError } from "@trpc/server";
+import ServerError from "~/utilities/error";
 
-type test = UserType
-
-import { unGuardedPrisma } from '@config/db'
+type test = UserType;
 
 export const userRouter = router({
   // GET http://localhost:3000/trpc/user.getOne?input="test"
-  getMany: publicProcedure.input(z.string()).query(async (opts) => {
-    opts.input // string
-    const stuff = await User.getMany(opts.ctx.req, {})
-    const stuff1: test[] = await unGuardedPrisma.user.findMany()
-    return stuff1
-    // opts.input // string
-    // return await User.getMany(opts.ctx.req, {})
-    // try {
-    //   return await User.getMany(opts.ctx.req, {})
-    //   return unGuardedPrisma.user.findMany()
-    // } catch (error) {
-    //   if (error instanceof ServerError) {
-    //     throw new TRPCError({ code: error.code, message: error.message })
-    //   } else {
-    //     throw error
-    //   }
-    // }
+  getMany: publicProcedure.query(async (opts) => {
+    try {
+      return await User.getMany(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
   }),
-  getOne: publicProcedure.input(z.string()).query((opts) => {
-    opts.input // string
-    return { response: 'user getOne' }
+  getOne: publicProcedure.query(async (opts) => {
+    try {
+      return await User.getOne(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
   }),
   // POST http://localhost:3000/trpc/user.createOne with body { name: "test" }
-  createOne: publicProcedure
-    .input(z.object({ name: z.string().min(5) }))
-    .mutation(async (opts) => {
-      // use your ORM of choice
-      return { response: 'user createOne' }
-    }),
-})
+  createOne: publicProcedure.mutation(async (opts) => {
+    try {
+      return await User.createOne(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
+  }),
+
+  createMany: publicProcedure.mutation(async (opts) => {
+    try {
+      return await User.createMany(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
+  }),
+
+  updateOne: publicProcedure.mutation(async (opts) => {
+    try {
+      return await User.updateOne(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
+  }),
+
+  updateMany: publicProcedure.mutation(async (opts) => {
+    try {
+      return await User.updateMany(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
+  }),
+
+  deleteOne: publicProcedure.mutation(async (opts) => {
+    try {
+      return await User.deleteOne(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
+  }),
+
+  deleteMany: publicProcedure.mutation(async (opts) => {
+    try {
+      return await User.deleteMany(opts.ctx.req, opts.input);
+    } catch (error) {
+      if (error instanceof ServerError) {
+        throw new TRPCError({ code: error.code, message: error.message });
+      } else {
+        throw error;
+      }
+    }
+  }),
+});
